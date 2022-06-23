fc9a6718b71d309b34dbebd250ce943a094cea8e
Ole Arends <olearends@web.de>
2022-06-23 14:07:47 +0200

    Modify getThrow.py to get working Generate CSV-files for all Throws (boundary probably too low -> should be repeated)

--- MotionAnalytica/getThrow.py
+++ MotionAnalytica/getThrow.py
@@ -4,17 +4,23 @@ import matplotlib.pyplot as plt
 import os
 import os
 
+### Mode-Adjuster ###
+
+filemode = 1  # 1 for whole folder, 0 for one file
+savemode = "csv"  # "plot" for saving plots, "csv" for saving csvs
+
+######
+
 
 column_x = 'accelerometerAccelerationX(G)'
 column_y = 'accelerometerAccelerationY(G)'
 column_z = 'accelerometerAccelerationZ(G)'
 timestamp = 'accelerometerTimestamp_sinceReboot(s)'
-workmode = 0 #1 for whole folder, 0 for one file
 dirPath = ""
 count = 1
 
 
-def findFile():
+def findFile(filemode):
     x = 1
     while x == 1:
         global dirPath
@@ -24,21 +30,22 @@ def findFile():
             print("Ordner existiert nicht")
         else:
             x = 0
-            if workmode == 0:
+            if filemode == 0:
                 file = input('Welche Datei soll verwendet werden? ')
                 file_list = file
-                #files = dirPath + "/csv/" + file
+                # files = dirPath + "/csv/" + file
             else:
                 file_list = os.listdir(dirPath)
-                #files = [dirPath+ "/csv/" + file for file in os.listdir(dirPath)]
-                #print(files)
-        #print(files)
+                # files = [dirPath+ "/csv/" + file for file in os.listdir(dirPath)]
+                # print(files)
+        # print(files)
         return file_list
 
+
 def readCsv(file):
-    #print(file)
+    # print(file)
     print(dirPath + file)
-    if os.path.isfile(dirPath +  file):
+    if os.path.isfile(dirPath + file):
         df = pd.read_csv(dirPath + file)
         # df.info()
         x = 0
@@ -47,9 +54,16 @@ def readCsv(file):
         print("Falscher Dateinname")
     return df
 
+
 def findThrow(df_raw):
-    clap_pos = df_raw[df_raw[column_z].gt(5)].index[0]
-    clap_neg = df_raw[df_raw[column_z].le(-5)].index[0]
+    try:
+        clap_pos = df_raw[df_raw[column_z].gt(5)].index[0]
+    except:
+        clap_pos = 9999999
+    try:
+        clap_neg = df_raw[df_raw[column_z].le(-5)].index[0]
+    except:
+        clap_neg = 9999999
     if clap_neg > clap_pos:
         clap = clap_pos
     else:
@@ -60,6 +74,7 @@ def findThrow(df_raw):
     df = df_raw[throwStart:throwEnd]
     return df
 
+
 def genPlot(df, file):
     modus = "Beschleunigung (Wurf)"
     plt.title(modus + ": " + file)
@@ -68,17 +83,19 @@ def genPlot(df, file):
     plt.plot(df[timestamp], df[column_z], label='z')
     plt.legend()
 
+
 def saveCsv(df, file):
     global count
-    csvDirPath = dirPath + "/throwCsv/"
+    csvDirPath = dirPath[:-5] + "/throwCsv/"
     if not os.path.isdir(csvDirPath):
         os.mkdir(csvDirPath)
         print("csvThrow Ordner erstellt!")
-    csvPath = csvDirPath + file[:-4] + "_" + "Beschleunigung (Wurf)" + ".csv"
-    df.to_csv(os.PathLike(csvPath))
+    csvPath = csvDirPath + file[:-4] + "_" + "Beschleunigung_(Wurf)" + ".csv"
+    df.to_csv(csvPath)
     if os.path.isfile(csvPath):
         print(str(count) + ": CSV erfolgreich gespeichert!")
     count += 1
+
 
 def savePng(file):
     global count
@@ -93,26 +110,37 @@ def savePng(file):
     count += 1
     plt.close()
 
-def lost():
-    if workmode == 0:
-        save(0)
-    elif workmode == 1:
-        filecount = 0
-        for file in file:
-            filecount += 1
-            save(filecount)
 
 def main():
-    if workmode == 0:
-        file = findFile()
-        genPlot(findThrow(readCsv(file)), file)
-        plt.show()
+    if filemode == 0:
+        try:
+            if savemode == "csv":
+                file = findFile(filemode)
+                saveCsv(findThrow(readCsv(file)), file)
+            elif savemode == "plot":
+                file = findFile(filemode)
+                genPlot(findThrow(readCsv(file)), file)
+                savePng(file)
+                plt.close()
+        except Exception as e:
+            print (e)
 
-    #elif workmode == 1:
+    elif filemode == 1:
+        files = findFile(filemode)
+        if savemode == "csv":
+            for file in files:
+                try:
+                    saveCsv(findThrow(readCsv(file)), file)
+                except Exception as e:
+                    print(e)
+        elif savemode == "plot":
+            for file in files:
+                try:
+                    genPlot(findThrow(readCsv(file)), file)
+                    savePng(file)
+                    plt.close()
+                except Exception as e:
+                    print(e)
 
 
 main()
-
-
-
-
